4426667c798d621a4b0febc22609ec4c73c31978
zkcpps <1129820657@qq.com>
2021-07-20 23:18:00 +0800

    '发布代码'

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Button } from './Button';
-export { default as Modal } from './Modal';
-export { default as Icon } from './Icon';
-export { default as Message } from './Message';
