030ec2d275017a719d9b78d3b486e1d5db857bf7
zkcpps <46256924+zkcpps@users.noreply.github.com>
2022-12-08 12:10:13 +0800

    1

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,8 @@ let BaseUrl = ''
 export default defineConfig({
   mode: 'site', // site: 站点模式（导航头 + 左侧菜单 + 右侧内容）。 doc：文档
   title: 'kely UI',
-  favicon: '/myui.png',
-  logo: '/myui.png', 
+  favicon: '/images/myui.png',
+  logo: 'images/myui.png', 
   outputPath: 'docs-dist',
   description: '自定义前端组件',
   // more config: https://d.umijs.org/config
