3ee29ef6961d1ec9235af16b5d7484d161edcc5b
zkcpps <46256924+zkcpps@users.noreply.github.com>
2022-12-08 14:08:43 +0800

    测试logo图片

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   mode: 'site', // site: 站点模式（导航头 + 左侧菜单 + 右侧内容）。 doc：文档
   title: 'kely UI',
   favicon: '/images/myui.png',
-  logo: '/images/myui.png', 
+  logo: '/myui.png', 
   outputPath: 'docs-dist',
   description: '自定义前端组件',
   // more config: https://d.umijs.org/config
