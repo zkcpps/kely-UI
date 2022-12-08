7c96e3b29efc32db4d30bb987c6555e4772c2522
zkcpps <46256924+zkcpps@users.noreply.github.com>
2022-12-08 11:48:29 +0800

    修改图片路径

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'dumi';
 
-let BaseUrl = ''
+let BaseUrl = '/kely-UI'
 export default defineConfig({
   mode: 'site', // site: 站点模式（导航头 + 左侧菜单 + 右侧内容）。 doc：文档
   title: 'kely UI',
